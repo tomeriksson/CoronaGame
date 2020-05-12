8545c6e2213b41f05f38dba8cd57019780fc7bc6
Gustav H <hultgrengustav@gmail.com>
2020-05-12 17:05:59 +0200

    Fienden åker till spelarens ursprungsposition men spelarens position uppdateras inte

---
+++ Game/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    Transform player;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GameObject.FindGameObjectWithTag("Player").activeInHierarchy)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+
+        agent = gameObject.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.transform.LookAt(player);
+        agent.destination = player.position;
+        print(player.position);
+    }
+}
