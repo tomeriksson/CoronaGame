d7d55cc2ffda38bf70e9b996b94d2ec8d9f545a9
kajsapihl <kajsafp@hotmail.com>
2020-05-14 10:19:28 +0200

    La till liv hos spelaren

---
+++ Game/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float Health;
+    public float HealthMax=100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health=100f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Heal(0.01f);
+    }
+
+    void Damage(float amount)
+    {
+        Health-=amount;
+        if(Health<=0){
+            Die();
+        }
+    }
+    void Heal(float amount){
+        if(Health<HealthMax){
+            Health+=amount;
+        }
+    }
+    void Die(){
+        Destroy(gameObject);
+    }
+/*    void OnCollisionEnter(Collision col){
+        if(col.gameObject.tag == "Enemy"){
+            Damage(5f);
+
+      }
+    }
+    */
+    void OnControllerColliderHit(ControllerColliderHit hit){
+        if(hit.gameObject.tag == "Enemy"){
+            Damage(5f);
+        }
+    }
+}
