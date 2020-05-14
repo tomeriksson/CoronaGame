8ed3edd332ccc5708b6df7794a530826d2229e40
unknown <viktor.werngren97@gmail.com>
2020-05-14 12:07:11 +0200

    Spawning

---
+++ Game/Assets/Scripts/Spawning.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Spawning : MonoBehaviour
+{
+
+    private float spawnTime;
+    [SerializeField]
+    private GameObject coronaPrefab;
+    [SerializeField]
+    private float spawnDelay;
+    public int enemyCounter;
+    public int enemiesOnField;
+    private int enemiesSpawnedThisRound = 0;
+
+    private int newRoundTime = 10;
+    private int newRoundDelay = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ShouldSpawn()){
+            Spawn();
+            enemiesSpawnedThisRound++;
+        }
+    }
+
+    private void Spawn()
+    {
+        spawnTime = Time.time + spawnDelay;
+        Instantiate(coronaPrefab, transform.position, transform.rotation, transform);
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time >= spawnTime && enemiesSpawnedThisRound < enemyCounter;
+    }
+
+    private void NewRound()
+    {
+
+    }
+
+    private void ShouldNewRound()
+    {
+        return Time.time >= spawnTime && enemiesSpawnedThisRound < enemyCounter;
+    }
+}
