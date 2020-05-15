a2c9b0d49f550db48a408dbc3f46d80426ce7f72
kajsapihl <kajsafp@hotmail.com>
2020-05-15 10:21:03 +0200

    Lagt till healthbar för spelaren, påbörjat healtbar till enemy prefab också

--- Game/Assets/Scripts/PlayerHealth.cs
+++ Game/Assets/Scripts/PlayerHealth.cs
@@ -1,26 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float Health;
     public float HealthMax=100f;
+    public Slider slider;
+    public Gradient gradient;
+    public Image fill;
     // Start is called before the first frame update
     void Start()
     {
         Health=100f;
+        slider.maxValue=Health;
+        slider.value=Health;
+        fill.color=gradient.Evaluate(1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Heal(0.01f);
+        Heal(0.05f);
+        slider.value=Health;
+        fill.color=gradient.Evaluate(slider.normalizedValue);
     }
 
     void Damage(float amount)
     {
         Health-=amount;
+        fill.color=gradient.Evaluate(slider.normalizedValue);
         if(Health<=0){
             Die();
         }
